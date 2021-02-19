13f2de46e20db2d71282d1b4e0a498a477d2ac4c
Brian <briancbarrow@gmail.com>
2021-02-18 17:19:44 -0700

    Added Sign in Page

---
+++ src/views/Test.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Test</router-link>
+    <router-link to="/signin">Sign In</router-link>
+  </div>
+  <h1>TEST</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"
+      >Vite Documentation</a
+    >
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
